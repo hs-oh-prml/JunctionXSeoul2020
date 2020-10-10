56dc7080044f150a894f029fcd9ef723dada31c6
kimbsu00 <58127442+kimbsu00@users.noreply.github.com>
2020-10-10 13:24:31 +0900

    UI

--- JunctionXSeoul2020/app/src/main/java/com/example/junctionxseoul2020/data/Post.kt
+++ JunctionXSeoul2020/app/src/main/java/com/example/junctionxseoul2020/data/Post.kt
@@ -3,12 +3,16 @@ package com.example.junctionxseoul2020.data
 import java.io.Serializable
 import java.util.*
 
+/*
+    uploadTime -> YYYY.MM.DD HH:mm
+*/
+
 data class Post(
     val pID: String,
     val img: Int,
     val uID: String,
     val story: String,
-    val uploadTime: Int,
+    val uploadTime: String,
     val uploadLat: Double,
     val uploadLng: Double,
     val comments: Vector<String>
